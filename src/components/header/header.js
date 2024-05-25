fccb86947ac35cddd93fc119e0947e9f87dae8be
halfmoon-mind <5338095@gmail.com>
2024-05-26 02:29:29 +0900

    feat: onclick logo to go main

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,59 +2,64 @@ import styled from "styled-components";
 import { ReactComponent as LogoSvg } from "../../assets/logo.svg";
 
 const Header = () => {
-  return (
-    <Container>
-      <LeftContainer>
-        <HeaderItem>으리으리 이야기</HeaderItem>/<HeaderItem>경신스</HeaderItem>
-      </LeftContainer>
-      <Logo>
-        <LogoSvg />
-      </Logo>
-      <SubscribeButton onClick={() => {}}>[세줄 경제] 구독하기</SubscribeButton>
-    </Container>
-  );
+    return (
+        <Container>
+            <LeftContainer>
+                <HeaderItem>으리으리 이야기</HeaderItem>/<HeaderItem>경신스</HeaderItem>
+            </LeftContainer>
+            <Logo>
+                <LogoSvg
+                    onClick={() => {
+                        window.location.href = "/";
+                    }}
+                    style={{ cursor: "pointer" }}
+                />
+            </Logo>
+            <SubscribeButton onClick={() => {}}>[세줄 경제] 구독하기</SubscribeButton>
+        </Container>
+    );
 };
 export default Header;
 
 const Container = styled.div`
-  padding-left: 20px;
-  padding-right: 20px;
-  height: 60px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  box-shadow: 0 0.5px rgba(0, 0, 0, 0.1);
+    padding-left: 20px;
+    padding-right: 20px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    box-shadow: 0 0.5px rgba(0, 0, 0, 0.1);
 `;
 
 const LeftContainer = styled.div`
-  display: flex;
-  gap: 20px;
+    display: flex;
+    gap: 20px;
 `;
 
 const HeaderItem = styled.div`
-  font-size: 16px;
-  font-weight: 600;
-  color: black;
-  cursor: pointer;
-  &:hover {
-    color: rgba(25, 25, 27, 0.5);
-  }
+    font-size: 16px;
+    font-weight: 600;
+    color: black;
+    cursor: pointer;
+    &:hover {
+        color: rgba(25, 25, 27, 0.5);
+    }
 `;
 
 const Logo = styled.div`
-  font-size: 24px;
-  font-weight: bold;
+    font-size: 24px;
+    font-weight: bold;
 `;
 
 const SubscribeButton = styled.button`
-  border-radius: 8px;
-  background-color: black;
-  color: white;
-  font-size: 12px;
-  padding: 10px 20px;
-  border: none;
-  cursor: pointer;
-  &:hover {
-    background-color: grey;
-  }
+    border-radius: 8px;
+    background-color: black;
+    color: white;
+    font-size: 12px;
+    padding: 10px 20px;
+    border: none;
+    cursor: pointer;
+    &:hover {
+        background-color: grey;
+    }
 `;
