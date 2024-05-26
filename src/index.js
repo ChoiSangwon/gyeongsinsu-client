a995943622d7d19a6b8a1689ac43d9e1fc9aadd3
halfmoon-mind <5338095@gmail.com>
2024-05-26 11:55:32 +0900

    feat: add FIREBASE GA

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import { app, analytics } from "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
+    <RecoilRoot>
+        <App />
+    </RecoilRoot>
 );
 
 // If you want to start measuring performance in your app, pass a function
